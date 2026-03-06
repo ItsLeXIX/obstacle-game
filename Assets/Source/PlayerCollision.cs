e67cd705962f1cb049d5e137b06ce43a114fbc3c
Mohammad Parsa Farajzadeh Jalali <if23b139@technikum-wien.at>
2026-03-06 13:27:43 +0100

    Game Over UI

--- Assets/Source/PlayerCollision.cs
+++ Assets/Source/PlayerCollision.cs
@@ -1,14 +1,21 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement playerMovement;
+    public TextMeshProUGUI gameOver;
     private void OnCollisionEnter(Collision other)
     {
         if (other.collider.tag == "Obstacle")
         {
-            playerMovement.forwardForce = 0;
+            playerMovement.enabled = false;
+            GameOver();
         }
+    }
+    public void GameOver()
+    {
+        gameOver.gameObject.SetActive(true);
     }
 }
